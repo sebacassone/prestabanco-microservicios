ae07dfd13ea432e643183f364b68be4688ad49cf
Sebastián <sebastian.gonzalez.cass@usach.cl>
2024-12-11 03:52:53 -0300

    feat: deployment

--- deployments.sh
+++ deployments.sh
@@ -5,9 +5,24 @@ kubectl delete services --all
 kubectl delete pods --all
 kubectl delete deployments --all
 
-kubectl apply -f config-server-deployment.yaml
+# Microservices Patterns
+kubectl apply -f config-deployment.yaml
 kubectl apply -f config-secrets.yaml
-kubectl apply -f eureka-server-deployment.yaml
-kubectl apply -f gateway-server-deployment.yaml
+kubectl apply -f eureka-deployment.yaml
+kubectl apply -f gateway-deployment.yaml
 kubectl apply -f jenkins-deployment.yaml
-kubectl apply -f prueba-server-deployment.yaml
+kubectl apply -f prueba-deployment.yaml
+
+# Database
+kubectl apply -f postgres-secrets.yaml
+kubectl apply -f postgres-deployment.yaml
+
+# Microservices
+kubectl apply -f addresses-deployment.yaml
+kubectl apply -f debts-deployment.yaml
+kubectl apply -f documents-deployment.yaml
+kubectl apply -f loans-deployment.yaml
+kubectl apply -f users-deployment.yaml
+
+# Frontend
+kubectl apply -f frontend-deployment.yaml
--- docker.sh
+++ docker.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-cd config-server
-mvn clean install -DskipTests=True
-docker build -t sebacassone/config-server .
-docker push sebacassone/config-server
+# cd config-server
+# mvn clean install -DskipTests=True
+# docker build -t sebacassone/config-server .
+# docker push sebacassone/config-server
 
-cd ../eureka-server
-mvn clean install -DskipTests=True
-docker build -t sebacassone/eureka-server .
-docker push sebacassone/eureka-server
+# cd ../eureka-server
+# mvn clean install -DskipTests=True
+# docker build -t sebacassone/eureka-server .
+# docker push sebacassone/eureka-server
 
-cd ../gateway-server
-mvn clean install -DskipTests=True
-docker build -t sebacassone/gateway-server .
-docker push sebacassone/gateway-server
+# cd ../gateway-server
+# mvn clean install -DskipTests=True
+# docker build -t sebacassone/gateway-server .
+# docker push sebacassone/gateway-server
 
-cd ../prueba-server
-mvn clean install -DskipTests=True
-docker build -t sebacassone/prueba-server .
-docker push sebacassone/prueba-server
+# cd ../prueba-server
+# mvn clean install -DskipTests=True
+# docker build -t sebacassone/prueba-server .
+# docker push sebacassone/prueba-server
 
-cd ../address-server
-gradlew clean build
-docker build -t sebacassone/address-server .
-docker push sebacassone/address-server
+# cd ../addresses-server
+# ./gradlew clean build -x test
+# docker build -t sebacassone/addresses-server .
+# docker push sebacassone/addresses-server
 
-cd ../debts-server
-gradlew clean build
-docker build -t sebacassone/debts-server .
-docker push sebacassone/debts-server
+# cd ../debts-server
+# ./gradlew clean build -x test
+# docker build -t sebacassone/debts-server .
+# docker push sebacassone/debts-server
 
-cd ../documents-server
-gradlew clean build
-docker build -t sebacassone/documents-server .
-docker push sebacassone/documents-server
+# cd ../documents-server
+# ./gradlew clean build -x test
+# docker build -t sebacassone/documents-server .
+# docker push sebacassone/documents-server
 
-cd ../loans-server
-gradlew clean build
-docker build -t sebacassone/loans-server .
-docker push sebacassone/loans-server
+# cd ../loans-server
+# ./gradlew clean build -x test
+# docker build -t sebacassone/loans-server .
+# docker push sebacassone/loans-server
 
-cd ../users-server
-gradlew clean build
-docker build -t sebacassone/users-server .
-docker push sebacassone/users-server
+# cd ../users-server
+# ./gradlew clean build -x test
+# docker build -t sebacassone/users-server .
+# docker push sebacassone/users-server
+
+cd ../Monolítica/frontend/
+docker build --build-arg VITE_PAYROLL_BACKEND_SERVER=localhost:30502 -t sebacassone/frontend-server .
+docker push sebacassone/frontend-server
